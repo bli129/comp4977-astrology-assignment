b5871e8b9844048315c344a2f3794233c5d7ff27
Benny Li <bennyli.byli@gmail.com>
2024-09-27 14:20:39 -0700

    Initial Commit

---
+++ AstrologyApp/AstrologyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstrologyAppApp.swift
+//  AstrologyApp
+//
+//  Created by Benny Li on 2024-09-27.
+//
+
+import SwiftUI
+
+@main
+struct AstrologyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstrologyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AstrologyApp
+//
+//  Created by Benny Li on 2024-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
