570e79a74d102ab3477ec7628f1e7a3cffb39424
mikaem <mkmanaligod@gmail.com>
2024-10-08 10:31:46 -0700

    add about page link in home view

--- AstrologyApp/AboutView.swift
+++ AstrologyApp/AboutView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 15) {
+            Text("Team Members")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 10)
+
+            VStack(alignment: .leading, spacing: 8) {
+                HStack {
+                    Text("Benny Li")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Text("A01315151")
+                        .foregroundColor(.gray)
+                }
+                HStack {
+                    Text("Mika Manaligod")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Text("A01332836")
+                        .foregroundColor(.gray)
+                }
+
+            }
+            .padding()
+            .cornerRadius(10)
+        }
+        .padding()
     }
 }
 
